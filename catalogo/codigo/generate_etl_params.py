1210182a51168395c435c08a9404f2002feae41c
Agustín Benassi <agusbenassi@gmail.com>
2017-07-15 13:25:39 -0300

    Cambio permisos de los archivos y genero parametros del ETL sin necesidad de la hoja "Parametros ETL".

--- catalogo/codigo/generate_etl_params.py
+++ catalogo/codigo/generate_etl_params.py
@@ -12,18 +12,65 @@ import sys
 
 import pandas as pd
 
-PARAMS_SHEET_NAME = "Parametros ETL"
+from helpers import find_ws_name
+
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(
+    os.path.abspath(__file__))))
+REPORTES_DIR = os.path.join(PROJECT_DIR, "catalogo", "datos", "reportes")
+PARAMS_FIELDS = [
+    "distribution_iedFileURL", "distribution_iedFileSheet",
+    "distribution_identifier", "distribution_title", "field_title", "field_id",
+    "field_identifierCell", "field_dataStartCell"
+]
+
+
+def get_params_from_sheet(catalog_xlsx_path,
+                          params_sheet_name="Parametros ETL"):
+    return pd.read_excel(catalog_xlsx_path, params_sheet_name)
+
+
+def get_params_from_model(catalog_xlsx_path,
+                          distribution_sheet_name="distribution",
+                          field_sheet_name="field"):
+    distribution_cols = [
+        "distribution_iedFileURL", "distribution_iedFileSheet",
+        "distribution_identifier", "distribution_title", "dataset_identifier",
+        "dataset_title"
+    ]
+    field_cols = [
+        "field_title", "field_id", "field_identifierCell",
+        "field_dataStartCell", "dataset_identifier", "dataset_title",
+        "distribution_identifier", "distribution_title"
+    ]
+
+    df_distribution = pd.read_excel(catalog_xlsx_path, find_ws_name(
+        catalog_xlsx_path, "distribution"))[distribution_cols]
+    df_field = pd.read_excel(catalog_xlsx_path, find_ws_name(
+        catalog_xlsx_path, "field"))[field_cols]
+
+    merged = df_field[field_cols].merge(
+        df_distribution[distribution_cols], how="outer", indicator=True,
+        on=["dataset_identifier", "dataset_title",
+            "distribution_identifier", "distribution_title"]
+    )
+
+    # genera reportes
+    merged[merged['_merge'] == 'left_only'][PARAMS_FIELDS].to_excel(
+        os.path.join(REPORTES_DIR, "reporte-fields-sin-distribution.xlsx"),
+        encoding="utf-8", index=False)
+    merged[merged['_merge'] == 'right_only'][PARAMS_FIELDS].to_excel(
+        os.path.join(REPORTES_DIR, "reporte-distribution-sin-fields.xlsx"),
+        encoding="utf-8", index=False)
+
+    df_etl_params = merged[merged['_merge'] == 'both']
+
+    return df_etl_params
 
 
 def main(catalog_xlsx_path, excels_urls_path):
-    df = pd.read_excel(catalog_xlsx_path, PARAMS_SHEET_NAME)
-    params_fields = [
-        "distribution_iedFileURL", "distribution_iedFileSheet",
-        "distribution_timeIndexCell", "distribution_identifier",
-        "distribution_title", "field_title", "field_id",
-        "field_identifierCell", "field_dataStartCell"
-    ]
-    df[params_fields].to_csv(
+
+    df_etl_params = get_params_from_model(catalog_xlsx_path)
+    df_etl_params[PARAMS_FIELDS].to_csv(
         "catalogo/datos/etl_params.csv",
         encoding="utf8", index=False
     )
--- catalogo/codigo/helpers.py
+++ catalogo/codigo/helpers.py
@@ -7,19 +7,24 @@ from __future__ import unicode_literals
 from __future__ import print_function
 from __future__ import with_statement
 import os
+from openpyxl import load_workbook
 
 
 def get_ws_case_insensitive(wb, title):
+    """Devuelve una hoja en un workbook sin importar mayúsculas/minúsculas."""
+    return wb[find_ws_name(wb, title)]
+
+
+def find_ws_name(wb, name):
     """Busca una hoja en un workbook sin importar mayúsculas/minúsculas."""
+    if type(wb) == str or type(wb) == unicode:
+        wb = load_workbook(wb, read_only=True, data_only=True)
 
-    for modified_title in [title, title.upper(), title.lower(), title.title()]:
-        try:
-            return wb[modified_title]
+    for sheetname in wb.sheetnames:
+        if sheetname.lower() == name.lower():
+            return sheetname
 
-        except Exception as e:
-            continue
-
-    raise e
+    return None
 
 
 def row_from_cell_coord(coord):
