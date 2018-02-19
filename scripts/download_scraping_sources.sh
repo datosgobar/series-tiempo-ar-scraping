4ebf390adfd8017befd1f8283d12fd7c5413109b
federicotdn <federicotedin.ar@gmail.com>
2018-02-19 10:57:17 -0300

    Agrego manejo correcto de archivos vacios para download_scraping_sources.sh y download_distributions.sh

--- scripts/download_scraping_sources.sh
+++ scripts/download_scraping_sources.sh
@@ -2,17 +2,17 @@ sources_urls_path="$1"
 
 # descarga las fuentes de cada catálogo
 while read catalog_id url; do
+	if [ $catalog_id ] && [ $url ]; then
+		# crea la carpeta de las fuentes originales del catálogo
+		mkdir -p "data/input/catalog/$catalog_id/sources" ;
 
-	# crea la carpeta de las fuentes originales del catálogo
-	mkdir -p "data/input/catalog/$catalog_id/sources" ;
-
-	# chequea que la URL está disponible y saludable
-	status_code=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' $url)
-	if [ $status_code == "200" ] || [ $status_code == "000" ] || [ $status_code == "302" ]; then
-		echo "$catalog_id $url" ;
-		wget -N --tries=3 --directory-prefix="data/input/catalog/$catalog_id/sources" "$url" --no-check-certificate ;
-	else
-		echo "URL $url NO EXISTE" ;
+		# chequea que la URL está disponible y saludable
+		status_code=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' $url)
+		if [ $status_code == "200" ] || [ $status_code == "000" ] || [ $status_code == "302" ]; then
+			echo "$catalog_id $url" ;
+			wget -N --tries=3 --directory-prefix="data/input/catalog/$catalog_id/sources" "$url" --no-check-certificate ;
+		else
+			echo "URL $url NO EXISTE" ;
+		fi ;
 	fi ;
-
 done < "$sources_urls_path"
