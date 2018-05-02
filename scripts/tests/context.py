4fd0e05a381abd14923a85cd90b4c4123151d90e
federicotdn <federicotedin.ar@gmail.com>
2018-05-02 11:54:14 -0300

    Remuevo tests antiguos.

--- scripts/tests/context.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import os
-import sys
-sys.path.insert(0, os.path.abspath("."))
-sys.path.insert(0, os.path.abspath(".."))
-
-import scripts.data as data
-import scripts.paths as paths
