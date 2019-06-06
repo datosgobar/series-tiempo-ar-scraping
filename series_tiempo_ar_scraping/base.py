94d5d8df0287074903679c16d5f7f137b98a8aa5
Alan Sosa <asosa@bitson.group>
2019-06-06 14:38:48 -0300

    Se implementa la clase Catalogo, su preproceso, proceso, y postproceso.

--- series_tiempo_ar_scraping/base.py
+++ series_tiempo_ar_scraping/base.py
@@ -7,11 +7,44 @@ logging.basicConfig(level=logging.DEBUG)
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 CATALOGS_INDEX_PATH = os.path.join(ROOT_DIR, "config.yaml.sample")
 
+class Catalog():
+
+    def __init__(self, id_catalog, url, format, *args, **kwargs):
+        self.id_catalog = id_catalog
+        self.url = url
+        self.format = format
+
+        super().__init__()
+
+    def process(self):
+        self.preprocess()
+
+        logging.debug('ID: {}'.format(self.id_catalog))
+        logging.debug('URL: {}'.format(self.url))
+        logging.debug('Formato: {}'.format(self.format))
+
+        self.postprocess()
+
+    def preprocess(self):
+        logging.debug('>>> PREPROCESS CATALOG <<<')
+
+    def postprocess(self):
+        logging.debug('>>> POSTPROCESS CATALOG <<<')
 
 class ETL():
 
     def __init__(self, *args, **kwargs):
         self.catalogs = []
+
+        catalogs_from_config = self.get_catalogs_index()
+        for catalog in catalogs_from_config.keys():
+            self.catalogs.append(
+                Catalog(
+                    id_catalog=catalogs_from_config.get(catalog).get('id'),
+                    url=catalogs_from_config.get(catalog).get('url'),
+                    format=catalogs_from_config.get(catalog).get('formato')
+                )
+            )
 
         super().__init__()
 
@@ -21,11 +54,8 @@ class ETL():
     def process(self):
         self.preprocess()
 
-        catalogs = self.get_catalogs_index()
-        for catalog in catalogs.keys():
-            logging.debug('Catálogo: {}'.format(catalog))
-            logging.debug('URL: {}'.format(catalogs.get(catalog).get('url')))
-            logging.debug('Formato: {}'.format(catalogs.get(catalog).get('formato')))
+        for catalog in self.catalogs:
+            catalog.process()
 
         self.postprocess()
 
