44c95e4e68a117663c0dc372fd602ecaf71d7a33
Marcos Damián Mesmer y Rosset <marcosdmyr@gmail.com>
2019-06-26 10:58:22 -0300

    Implementa CSVReader en DirectDownloadProcessor

--- series_tiempo_ar_scraping/processors.py
+++ series_tiempo_ar_scraping/processors.py
@@ -1,12 +1,25 @@
 import logging
 
+from series_tiempo_ar.readers.csv_reader import CSVReader
+
 logging.basicConfig(level=logging.DEBUG)
 
 
 class DirectDownloadProcessor():
 
     def __init__(self, distribution_metadata):
+
         self.distribution_metadata = distribution_metadata
 
     def run(self):
-        return False, None
+        valid_df, distribution_df = False, None
+
+        try:
+            reader = CSVReader(self.distribution_metadata)
+            valid_df, distribution_df = True, reader.read()
+            logging.debug('>>> Descargó la distribución <<<')
+        except Exception:
+            logging.debug('>>> Falló la descarga de la distribución <<<')
+
+        return valid_df, distribution_df
+
