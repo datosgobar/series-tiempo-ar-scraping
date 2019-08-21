8025e08637e086f70bf1ae5be9157ebfbdde0eb1
Alan Sosa <alan.sosa@datalytics.com>
2019-08-21 12:41:15 -0300

    Se eliminan comentarios.

--- series_tiempo_ar_scraping/factories.py
+++ series_tiempo_ar_scraping/factories.py
@@ -7,37 +7,6 @@ import uuid
 from series_tiempo_ar_scraping.base import Catalog, Dataset, Distribution
 
 
-# class DistributionFactory(factory.Factory):
-
-#     class Meta:
-#         model = Distribution
-
-#     identifier = factory.Faker('uuid4')
-#     parent = None
-#     context = {}
-
-
-
-# class DatasetFactory(factory.Factory):
-
-#     class Meta:
-#         model = Dataset
-#         exclude = ('childs', 'report',)
-
-#     identifier = factory.Faker('uuid4')
-#     parent = None
-#     context = {}
-#     report = factory.Dict({
-#         'dataset_identifier': factory.SelfAttribute('..identifier'),
-#         'dataset_status': 'OK',
-#     })
-
-#     childs = factory.RelatedFactoryList(
-#         DistributionFactory,
-#         size=lambda: random.randint(1, 10),
-#     )
-
-
 class CatalogFactory(factory.Factory):
 
     class Meta:
@@ -46,12 +15,3 @@ class CatalogFactory(factory.Factory):
     identifier = factory.Faker('uuid4')
     parent = None
     context = {}
-    # pdb.set_trace()
-    # childs = factory.RelatedFactoryList(DatasetFactory, size=3, parent=parent)
-
-    # childs = factory.RelatedFactoryList(
-    #     DatasetFactory,
-    #     size=10,
-    #     # size=lambda: random.randint(0,10),
-    #     # parent=factory.LazyAttribute(lambda self: self),
-    # )
