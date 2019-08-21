fb113df04af9ddd591e2ce36ab98254bbcb32098
Alan Sosa <alan.sosa@datalytics.com>
2019-08-21 12:41:57 -0300

    Se agregan tests para las funciones de los indicadores de datasets, distributions, y el porcentaje de las distributions.

--- tests/test_catalogs.py
+++ tests/test_catalogs.py
@@ -1,7 +1,7 @@
 from mock import patch
 import pytest
 
-from series_tiempo_ar_scraping.base import Catalog
+from series_tiempo_ar_scraping.base import Catalog, ETLObject
 from series_tiempo_ar_scraping.factories import CatalogFactory
 
 
@@ -14,8 +14,56 @@ from series_tiempo_ar_scraping.factories import CatalogFactory
         ([{'identifier': 'foo', 'dataset_status': 'ERROR'}, {'identifier': 'bar', 'dataset_status': 'ERROR'}], 0),
     ],
 )
-def test_get_catalog_datasets_reports(catalog_datasets_reports, expected):
-    catalog = CatalogFactory()
+def test_get_catalog_dataset_reports_indicator(catalog_datasets_reports, expected):
+    with patch.object(
+        ETLObject,
+        'init_object',
+        return_value={}
+    ):
+        catalog = CatalogFactory()
 
-    catalog.context['catalog_datasets_reports'] = catalog_datasets_reports
-    assert catalog._get_datasets_reports(status='OK').get('datasets_ok') == expected
+        catalog.context['catalog_datasets_reports'] = catalog_datasets_reports
+        assert catalog._get_dataset_reports_indicator(status='OK') == expected
+
+
+@pytest.mark.parametrize(
+    'catalog_distributions_reports, expected',
+    [
+        ([], 0),
+        ([{'identifier': 'foo', 'distribution_status': 'OK'}, {'identifier': 'bar', 'distribution_status': 'OK'}], 2),
+        ([{'identifier': 'foo', 'distribution_status': 'OK'}, {'identifier': 'bar', 'distribution_status': 'ERROR'}], 1),
+        ([{'identifier': 'foo', 'distribution_status': 'ERROR'}, {'identifier': 'bar', 'distribution_status': 'ERROR'}], 0),
+    ],
+)
+def test_get_catalog_distribution_reports_indicator(catalog_distributions_reports, expected):
+    with patch.object(
+        ETLObject,
+        'init_object',
+        return_value={}
+    ):
+        catalog = CatalogFactory()
+
+        catalog.context['catalog_distributions_reports'] = catalog_distributions_reports
+        assert catalog._get_distribution_reports_indicator(status='OK') == expected
+
+
+@pytest.mark.parametrize(
+    'catalog_distributions_reports, expected',
+    [
+        ([], 0),
+        ([{'identifier': 'foo', 'distribution_status': 'OK'}, {'identifier': 'bar', 'distribution_status': 'OK'}], 100.0),
+        ([{'identifier': 'foo', 'distribution_status': 'OK'}, {'identifier': 'bar', 'distribution_status': 'ERROR'}], 50.0),
+        ([{'identifier': 'foo', 'distribution_status': 'ERROR'}, {'identifier': 'bar', 'distribution_status': 'ERROR'}], 0.0),
+        ([{'identifier': 'foo', 'distribution_status': 'OK (Replaced)'}, {'identifier': 'bar', 'distribution_status': 'ERROR'}], 50.0),
+    ],
+)
+def test_get_catalog_distributions_percentage_indicator(catalog_distributions_reports, expected):
+    with patch.object(
+        ETLObject,
+        'init_object',
+        return_value={}
+    ):
+        catalog = CatalogFactory()
+
+        catalog.context['catalog_distributions_reports'] = catalog_distributions_reports
+        assert catalog._get_distributions_percentage_indicator() == expected
